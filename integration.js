a79f695102f41b51aeab688b241c0bcfc30c1fa5
Ed <ed@polarity.io>
2022-07-28 14:42:37 -0400

    Update dependencies and switch to postman-request

--- integration.js
+++ integration.js
@@ -1,6 +1,6 @@
 const async = require('async');
 const config = require('./config/config');
-const request = require('request');
+const request = require('postman-request');
 const util = require('util');
 const url = require('url');
 const fs = require('fs');
